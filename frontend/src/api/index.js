0ff2a7b7755434f90312f237db4ba0d201669c98
hanzala135 <hanzalarashid35@gmail.com>
2026-05-10 15:36:14 +0500

    fir sb

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,18 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: BASE_URL,
 });
 
-
-
-export const getHistory    = ()   => api.get('/history').then(r => r.data);
-export const getStats      = ()   => api.get('/history/stats').then(r => r.data);
-export const getRun        = (id) => api.get(`/history/${id}`).then(r => r.data);
-export const deleteRun     = (id) => api.delete(`/history/${id}`).then(r => r.data);
-export const clearHistory  = ()   => api.delete('/history').then(r => r.data);
-
-
+export const getHistory = () => api.get("/history").then((r) => r.data);
+export const getStats = () => api.get("/history/stats").then((r) => r.data);
+export const getRun = (id) => api.get(`/history/${id}`).then((r) => r.data);
+export const deleteRun = (id) =>
+  api.delete(`/history/${id}`).then((r) => r.data);
+export const clearHistory = () => api.delete("/history").then((r) => r.data);
 
 export const startScrape = (task) =>
-  new EventSource(`http://localhost:5000/api/scrape/${task}`); 
+  new EventSource(`${BASE_URL}/scrape/${task}`);
